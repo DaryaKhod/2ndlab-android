e5d37366406142a1baa493d12c1a6b4bbf4c9129
DaryaKhod <70846539+DaryaKhod@users.noreply.github.com>
2022-12-05 22:02:32 +0300

    wasedrf

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,52 +1,98 @@
 package com.example.myapplication2;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.myapplication2.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.util.List;
+import java.util.Optional;
 
-    EditText email;
-    EditText password;
-    Button loginbutton;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText login_Text, pass_Text;
+    Button btn_Save, btn_Load, btn_Delete;
+    Toast toast;
+
+    DatabaseHandler db = new DatabaseHandler(this);
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        email=findViewById(R.id.emailid);
-        password=findViewById(R.id.editTextTextPassword2);
-        loginbutton = findViewById(R.id.button2);
-        loginbutton.setOnClickListener(this);
+
+        login_Text = (EditText) findViewById(R.id.emailid);
+        pass_Text = (EditText) findViewById(R.id.editTextTextPassword2);
+
+        btn_Save = (Button) findViewById(R.id.save_button);
+        btn_Save.setOnClickListener(this);
+        btn_Load = (Button) findViewById(R.id.load_button);
+        btn_Load.setOnClickListener(this);
+        btn_Delete = (Button) findViewById(R.id.delete_button);
+        btn_Delete.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.button2:
-                String e = email.getText().toString();
-                String p = password.getText().toString();
-                if ((e.equals("email.com"))&(p.equals("123"))){
-                    Intent i = new Intent(this, MyArrayList.class);
-                    i.putExtra("e", e);
-                    startActivity(i);
-                }else  {
-                    Toast.makeText(MainActivity.this, "Пароль и почта введены с ошибкой!", Toast.LENGTH_LONG).show();
-                    email.setText("");
-                    password.setText("");
-                }
+    protected void onPause() {
+        super.onPause();
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.save_button:
+
+                if (db.addUser(new User(login_Text.getText().toString(), pass_Text.getText().toString()))){
+                    toast = Toast.makeText(getApplicationContext(),
+                            "Новый пользователь успешно сохранен", Toast.LENGTH_SHORT);
+                } else {
+                    toast = Toast.makeText(getApplicationContext(),
+                            "Пользователь с данным логином уже зарегестрирован", Toast.LENGTH_SHORT);
+                }
+                  toast.show();
+                break;
+            case R.id.delete_button:
+                db.deleteUser(login_Text.getText().toString());
+                toast = Toast.makeText(getApplicationContext(),
+                        "Пользователь удален", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case R.id.load_button:
+                List<User> allUsers = db.getAllUsers();
+                Optional<User> any = allUsers.stream().filter(i ->
+                        i.getLogin().equals(login_Text.getText().toString()) && i.getPassword().equals(pass_Text.getText().toString())
+                ).findAny();
+                if (any.isPresent()) {
+                    Intent i = new Intent(this, MyArrayList.class);
+                    User user = any.get();
+                    i.putExtra("login", user.getLogin());
+                    i.putExtra("password", user.getPassword());
+                    startActivity(i);
+                } else {
+                    toast = Toast.makeText(getApplicationContext(),
+                            "Неверный логин/пароль", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
                 break;
             default:
                 break;
         }
-
     }
+
+
 }
